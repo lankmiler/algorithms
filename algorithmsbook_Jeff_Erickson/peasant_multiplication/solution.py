115b78cf1cfcd4daab2e0a389b97e54c17c06240
Pavel Lankmiler <pavel@incorporatenow.com>
2020-03-27 17:00:58 +0200

    Peasant multiplication

---
+++ algorithmsbook_Jeff_Erickson/peasant_multiplication/solution.py
@@ -0,0 +1,11 @@
+def peasant_multiplication(x, y):
+	prod = 0
+	while x > 0:
+		if x%2 != 0:
+			prod = prod + y
+		x = int(x / 2)
+		y = int(y + y)
+	return prod
+
+
+assert((123*456) == peasant_multiplication(123, 456))
